aef74568b16053f3b2203a4fb3d0d8e58a97c60e
harmeet88523 <hs.lamba541@gmail.com>
2015-02-18 01:38:33 +0530

    Commented - views, models. Added function skeleton to views.

--- main/models.py
+++ main/models.py
@@ -1,19 +1,31 @@
+#<complete> explain the schema of the database
+
+####################################################### IMPORT STATTEMENTS #########################################################
+
 from django.db import models
+
+####################################################### GLOBAL VARIABLES   #########################################################
 
 power_category_choices=(
 	('Atk','Attack'),
 	('Def','Defense'),
 )
+
+########################################################## MODLES ##################################################################
+
 # Create your models here.
+
 class Team(models.Model):
 	no=models.PositiveSmallIntegerField()
 	name=models.CharField(max_length=200)
 	money=models.PositiveIntegerField(default=0)
 	assets=models.PositiveIntegerField(default=0)
 	labour=models.PositiveIntegerField(default=0)
-	qs=models.ManyToManyField('Question')
-	aqs=models.ManyToManyField('Question')
+	net_worth=models.PositiveIntegerField(default=0)
+	qs=models.ManyToManyField('Question') #<optional> addonly if question wala thing is to be incorporated
+	aqs=models.ManyToManyField('Question') #<optional> addonly if question wala thing is to be incorporated
 	powers=models.ManyToManyField('Power')
+	cash_generated=models.PositiveIntegerField() # thsi store teh cash generated in the current round
 
 	def __unicode__(self):
 		return self.name + ' ' + self.member_set.filter(team_leader=True)[0].name
@@ -37,3 +49,9 @@ class Power(models.Model):
 
 	def __unicode__(self):
 		return self.category + ' ' + unicode(self.price) + ' - ' + unicode(self.quantity) + ' left'
+
+######################################################## OPTIONAL SEGMENT ##################################################################
+
+class Question(models.Model):
+	#<optional> complete only if the question wala thing is to be incorporated
+	#<complete>
--- main/views.py
+++ main/views.py
@@ -1,9 +1,23 @@
+# This one of my most documented views file
+# It is never too late to start a good habit
+
+##################################################### IMPORT STATEMENTS ###########################################################
+
 from django.shortcuts import render
+from main.models import *
+
+###################################################### GLOBAL VARIABLES ###########################################################
+
+asset_sellof_rate = #<complete>
+labour_sellof_rate = asset_sellof_rate #<complete>
+
+########################################################## VIEWS ####################################################################
 
 # Create your views here.
-def add_money(request):
+
+def add_value(request):
 	if request.method == 'POST':
-		form=AddMoneyForm(request.POST)
+		form=AddValueForm(request.POST)
 		if form.is_valid():
 			data=form.cleaned_data
 			for t in data['team_list']:	
@@ -11,4 +25,60 @@ def add_money(request):
 					team=Team.objects.get(no=t)
 				except : #<complete> object not found specific error
 					return HttpResponse('Team not found according to number')
-				
+				#<complete> for all assets money and labour
+
+#This view is when a team wants to invoke a use_power
+#This view checks whether a team that has a particular power has it or no
+#This view also checks whether the affetcted team has any safegaurds
+def use_power(request):
+	#<complete>
+
+# This views allows a team to buy a power
+# This view will check whether there is quantity left of that power
+# It also checks whether the team has enough money to buy the power
+def buy_power(request):
+	#<complete>
+
+# This is a function and not a django view
+# It is used to sell of a team's assets or labour
+# It will sell of at the predetermined rate
+def sell_commodities(term):
+	#<complete>
+
+# This view allows a team to sell of its commodities
+# It will ensure that the team has the quantity that it intends to sell
+def sell(request):
+	#<complete>
+
+# This is a view which is called when a round gets over
+# It will automatically generate cash for a team at the end
+# It will calculate the net worth of the company at the end of each round and store it in the database
+def round_completion(request):
+	#<complete>
+
+# This is a function and not a view 
+# This is used to calculate the net worth of a team and store it in the database
+# This function is invoked in almost every view to update the networth of the team
+def calculate_net_worth(team):
+	#<complete>
+
+# This is a an experiment from my side
+# This view is to revert back a mistake if made
+# This is done by generating a log and reverting back as and when required
+def revert_back(request):
+	#<complete>
+
+############################################# OPTIONAL SEGMENT ######################################################################
+
+# This view allows a team to buy a question
+# This view will also check whether the team has enough money to buy that question
+# It will also sell of the commodities of the company at the predecided rate to raise money to buy the question
+def buy_question(request):
+	#<complete>
+	#<optional> write only if the the question wala thing has to be incorporated
+
+# This view is called when a team answers a question
+# It checks whether an answer is right or not and accordingly awards the team
+def attempt_question(request):
+	#<complete>
+	#<optional> write only if the the question wala thing has to be incorporated
