cfd19c7dba313a5381f4322d5c2fe118b8601022
harmeet88523 <hs.lamba541@gmail.com>
2015-05-19 12:13:28 +0530

    Basic Implementation of ideas

--- main/models.py
+++ main/models.py
@@ -1,36 +1,43 @@
 #<complete> explain the schema of the database
 
-####################################################### IMPORT STATTEMENTS #########################################################
+####################################################### IMPORT STATTEMENTS ############################################################
 
 from django.db import models
 
-####################################################### GLOBAL VARIABLES   #########################################################
+####################################################### GLOBAL VARIABLES   ############################################################
 
 power_category_choices=(
-	('Atk','Attack'),
-	('Def','Defense'),
-)
+	('attack','Attack'),
+	('defense','Defense'),
+	)
 
-########################################################## MODLES ##################################################################
+commodity_choices=(
+	('money','Money'),
+	('assets','Assets'),
+	('labour','Labour'),
+	)
+
+########################################################## MODELS #####################################################################
 
 # Create your models here.
 
 class Team(models.Model):
-	no=models.PositiveSmallIntegerField()
+	no=models.PositiveSmallIntegerField(unique=True)
 	name=models.CharField(max_length=200)
 	money=models.PositiveIntegerField(default=0)
 	assets=models.PositiveIntegerField(default=0)
 	labour=models.PositiveIntegerField(default=0)
 	net_worth=models.PositiveIntegerField(default=0)
-	qs=models.ManyToManyField('Question') #<optional> addonly if question wala thing is to be incorporated
-	aqs=models.ManyToManyField('Question') #<optional> addonly if question wala thing is to be incorporated
+	# qs=models.ManyToManyField('Question') #<optional> addonly if question wala thing is to be incorporated
+	# aqs=models.ManyToManyField('Question') #<optional> addonly if question wala thing is to be incorporated
 	powers=models.ManyToManyField('Power')
 	cash_generated=models.PositiveIntegerField() # thsi store teh cash generated in the current round
+	is_eliminated = models.BooleanField(default=False)
 
 	def __unicode__(self):
 		return self.name + ' ' + self.member_set.filter(team_leader=True)[0].name
 
-class Member(models.Model):
+class Participant(models.Model):
 	team=models.ForeignKey('Team')
 	name=models.CharField(max_length=200)
 	email=models.EmailField()
@@ -42,7 +49,8 @@ class Member(models.Model):
 		return self.name + ' ' + self.team.name
 
 class Power(models.Model):
-	category=models.CharField(max_length=3,choices=power_category_choices)
+	category=models.CharField(max_length=8,choices=power_category_choices)
+	commodity=models.CharField(max_length=6,choices=commodity_choices)
 	price=models.PositiveIntegerField()
 	strength=models.PositiveIntegerField()
 	quantity=models.PositiveSmallIntegerField()
@@ -52,6 +60,7 @@ class Power(models.Model):
 
 ######################################################## OPTIONAL SEGMENT ##################################################################
 
-class Question(models.Model):
+'''class Question(models.Model):
 	#<optional> complete only if the question wala thing is to be incorporated
 	#<complete>
+	'''
--- main/views.py
+++ main/views.py
@@ -8,36 +8,116 @@ from main.models import *
 
 ###################################################### GLOBAL VARIABLES ###########################################################
 
-assets_sellof_rate = #<complete>
-labour_sellof_rate = assets_sellof_rate #<complete>
+sell_dict={'assets' : ,'labour' :  }
+
 # Here nw - networth
-assets_nw_rate=1 #<complete>
-labour_nw_rate=1 #<complete>
+nw_rate_dict={}
+nw_rate_dict['assets']=1 #<complete>
+nw_rate_dict['labour']=1 #<complete>
+
 #Here cg - cash generation
-assets_cg_rate=1 #<complete>
-labour_cg_rate=1 #<complete>
+cg_rate_dict = {}
+cg_rate_dict['assets']=1 #<complete>
+cg_rate_dict['labour']=1 #<complete>
+
+#This stores the minimum limit below which a team cannot sell its commodities
+min_lim={}
+min_lim['assets']=0 #<complete>
+min_lim['labour']=0 #<complete>
+
+#This is the minimum limit that one has to kept while initially distributing the commodities
+i_min_limit={}
+i_min_limit['assets']=0 #<complete>
+i_min_limit['labour']=0 #<complete>
+
+initial_value=500000 #<complete>
 
 ########################################################## VIEWS ####################################################################
 
 # Create your views here.
 
-def add_value(request):
+#This view is used to add a new team to the database
+#This view also checks whether it isnt less than the minimum amount decided the initial amount as decided
+def add_team():
+	
+
+
+#This view is used to add value to a particular team
+def add_value(request):#<done>
 	if request.method == 'POST':
 		form=AddValueForm(request.POST)
 		if form.is_valid():
 			data=form.cleaned_data
-			for t in data['team_list']:	
-				try :
-					team=Team.objects.get(no=t)
-				except : #<complete> object not found specific error
-					return HttpResponse('Team not found according to number')
-				#<complete> for all assets money and labour
+			add_commodity(data['team'],data['commodity'],data['amount'])
+			calculate_net_worth(data['team'])
+		else:
+			return HttpResponse('Invalid')#<complete> 
+	else:
+		raise Http404 #<complete>
 
-#This view is when a team wants to invoke a use_power
+#TC: This is a python function and not a view
+#This function adds value to the particular commodity of a team
+def add_commodity(team,term,amount):
+	final_amount=getattr(team,term) + amount
+	setattr(team,term,amount)
+	return
+
+#This view is when a team wants to invoke a power
 #This view checks whether a team that has a particular power has it or no
-#This view also checks whether the affetcted team has any safegaurds
+#This view also checks whether the affected team has any safegaurds
 def use_power(request):
-	#<complete>
+	if request.method =='POST':
+		form=UsePowerForm(request.POST)
+		if form.is_valid():
+			data=form.cleaned_data
+			try : 
+				using_team = Team.objects.get(data['using_team']) # This is the team which is using the power 
+			except: #<complete> specific error code
+				return HttpResponse('No such team exists') #<complete> check whether any json needed
+			try : 
+				used_team = Team.objects.get(data['used_team']) # This is the team on which the power is being used
+			except: #<complete> specific error code
+				return HttpResponse('No such team exists') #<complete> check whether any json needed
+			
+			# Checking if either of teams is eliminated
+
+			if using_team.is_eliminated == True or used_team.is_eliminated == True:
+				return HttpResponse('One of the teams is already eliminated') #<complete> check whether any json needed
+
+			# Checking if the 'using_team' has the power it wants to use
+
+			if not using_team.powers.filter(pk=data['power'].pk):
+				return HttpResponse('The team doesnt have this power') #<complete> check for json
+			
+			# Checking if the power is 'Attack' type or 'Defense' type
+
+			if data['power'].type == 'defense':
+				return HttpResponse('The power being used is of defense type') #<complete> check for json
+			
+			# Checking for safeguards, if any.
+			# Here the rule is if you have a safeguard of say x value and attack on you is of y value
+			# If x < y then x is fully used and the remainder damage is done.
+			# If x >= y then x is fully used and no damage is done.
+			# Also, multiple smaller safeguards can be used.
+			# If both less and greater than safeguards present then lesser than used first
+
+			safeguards_used_team = used_team.powers.filter(category='defense',commodity = data['power'].commodity)
+			safeguards_used_team_e = safeguards_used_team.filter(strength=data['power'].strength) # Here 'e' is for equal strength
+			safeguards_used_team_l = safeguards_used_team.filter(strength__lt=data['power'].strength).order_by('strength') # Here 'l' is for lesser strength
+			safeguards_used_team_g = safeguards_used_team.filter(strength__gt=data['power'].strength).order_by('strength') # Here 'g' is for greater strength
+
+			net_attack = 0 # Here this represents the net attack after safeguards
+			
+			if safeguards_used_team_e:
+				net_attack=0
+			elif safeguards_used_team_l:
+				for s in safeguards_used_team_l:
+					if 
+
+
+
+
+
 
 # This views allows a team to buy a power
 # This view will check whether there is quantity left of that power
@@ -93,35 +173,61 @@ def buy_power(request): #<partially done>
 # This is a function and not a django view
 # It is used to sell of a team's assets or labour
 # It will sell of at the predetermined rate
-def sell_commodities(term):
-	#<complete>
+# It will ensure that the team has the quantity that it intends to sell
+def sell_commodities(team,term,amount):#<done>
+	# Here is checks if the team has enough quantity to sell the respective commodity
+
+	if final_amount < min_lim[term]:
+		return False
+
+	final_amount=getattr(team,term) - amount
+	setattr(team,term,final_amount)
+	team.money += sell_dict[term] * amount
+	calculate_net_worth(team)
+	return True
 
 # This view allows a team to sell of its commodities
-# It will ensure that the team has the quantity that it intends to sell
-def sell(request):
-	#<complete>
+def sell(request):#<partially done>
+	if request.method == 'POST':
+		form = SellCommoditiesForm(request.POST)
+		if form.is_valid():
+			data = form.cleaned_data
+			if data['team'].is_eliminated == True:
+				return HttpResponse('team already eliminated')
+
+			if not sell_commodities(data['team'],data['commodity'],data['amount']):
+				return HttpResponse('Cant Sell any more of ' + data['commodity']) #<complete> check for json response
+			else :
+				return HttpResponse('Done') #<complete> check for json response
+		else:
+			return HttpResponse('Invalid data sent') #<complete> check for json response
+	else:
+		raise Http404
 
 # This is a view which is called when a round gets over
 # It will automatically generate cash for a team at the end
 # It will calculate the net worth of the company at the end of each round and store it in the database
-def round_completion(request):
+def round_completion(request):#<partially done>
 	if request.method == 'POST':
 		team_list=Team.objects.filter(is_eliminated=False)
 		for team in team_list:
-			team.money += team.assets * assets_cg_rate + team.labour * labour_cg_rate
+			team.money += team.assets * cg_rate_dict['assets'] + team.labour * cg_rate_dict['labour']
 			calculate_net_worth(team)
-
+		return HttpResponse('Done') #<complete> send json
+	else :
+		raise Http404
 
 # This is a function and not a view 
 # This is used to calculate the net worth of a team and store it in the database
 # This function is invoked in almost every view to update the networth of the team
 def calculate_net_worth(team): #<done>
-	team.net_worth= team.money * money_nw_rate + team.assets * assets_nw_rate + team.labour * labour_nw_rate
+	team.net_worth= team.money * money_nw_rate + team.assets * nw_rate_dict['assets'] + team.labour * nw_rate_dict['labour']
 	team.save()
 	return
 
 ############################################# OPTIONAL SEGMENT ######################################################################
 
+'''
 # This is a an experiment from my side
 # This view is to revert back a mistake if made
 # This is done by generating a log and reverting back as and when required
@@ -140,3 +246,4 @@ def buy_question(request):
 def attempt_question(request):
 	#<complete>
 	#<optional> write only if the the question wala thing has to be incorporated
+'''
